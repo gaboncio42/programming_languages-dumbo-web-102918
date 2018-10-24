23290305e2e0db06bd91123bc7559c4b2a98930d
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-24 19:19:56 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,10 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, language|
+    if style == :00 && language == :ruby
+      new_hash[:ruby] = {:type => "interpreted"}
+    end
+  end
+  
+  new_hash
 end
